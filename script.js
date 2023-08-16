bd6c58e445486cc4411dc56bdf5be640a5cc1433
Dappa <daviddappa760@gmail.com>
2023-08-15 18:13:24 -0700

    feat: first push of completed website

---
+++ script.js
@@ -0,0 +1,76 @@
+AOS.init();
+
+
+const navBar = document.getElementById('nav'),
+navBarLink = document.querySelectorAll('.nav__link'),
+navBarOpen = document.querySelector('.nav__hamburger--open'),
+navBarClose = document.querySelector('.nav__hamburger--close'),
+navBarSmMd = document.querySelector('.nav__links--small--and--medium'),
+navLinkSmMd = document.querySelectorAll('.nav__link--small--and--medium'),
+navBarBorderBottom = document.querySelector('.nav__navbar--fixed');
+
+const talentCards = document.querySelector('.talent__cards');
+
+// Change background color of navbar on scroll
+
+window.addEventListener('scroll', () => {
+    const verticalScroll = window.scrollY;
+
+    if (verticalScroll < 200 ) {
+        navBar.style.backgroundColor = 'rgb(241, 237, 219)';
+        navBar.style.color = "#000";
+        // navBarLink.style.color = "#000"; 
+        navBarLink.forEach(link => {
+            link.style.color = "#000";
+        })
+        navLinkSmMd.forEach(link => {
+            link.style.color ="#000";
+        })
+    } else {
+        navBar.style.backgroundColor = '#000';
+        navBar.style.color = '#fff';
+        // navBarLink.style.color = "#fff";
+        navBarLink.forEach(link => {
+            link.style.color = "#fff";
+        })
+        navLinkSmMd.forEach(link => {
+            link.style.color ="#fff";
+        })
+    }
+})
+
+
+// Navigation bar functionality
+
+
+navBarOpen.addEventListener('click', () => {
+    navBarClose.style.display = 'block';
+    navBarOpen.style.display = 'none';
+    navBarSmMd.style.display = 'block';
+    navLinkSmMd.forEach(link => {
+        link.style.display = 'flex';
+        link.style.flexDirection = 'column';
+        link.style.justifyEnd = 'flex-end';
+    });
+    navBarBorderBottom.style.borderBottomStyle = 'solid';
+    navBarBorderBottom.style.borderBottomWidth = '1px';
+})
+
+navBarClose.addEventListener('click', () => {
+    navBarClose.style.display = 'none';
+    navBarOpen.style.display = 'block';
+    navBarSmMd.style.display = 'none';
+    navBarBorderBottom.style.borderBottomStyle = 'none';
+})
+
+navLinkSmMd.forEach(link => {
+    link.addEventListener('click', () => {
+        navBarClose.style.display = 'none';
+        navBarOpen.style.display = 'block';
+        navBarSmMd.style.display = 'none';
+        navBarBorderBottom.style.borderBottomStyle = 'none';
+    })
+})
+
+// Scroll events
+
