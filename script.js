ec3b2cb89673c75d085a4563aff385f85982510a
Dappa <daviddappa760@gmail.com>
2023-08-15 18:49:44 -0700

    feat: included position property for hamburger list

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ navBarOpen.addEventListener('click', () => {
     });
     navBarBorderBottom.style.borderBottomStyle = 'solid';
     navBarBorderBottom.style.borderBottomWidth = '1px';
+    navBarSmMd.style.position = 'absolute';
     navBarSmMd.style.zIndex = '10';
 })
 
